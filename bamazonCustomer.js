a397a50ad0861ee3b5b555a5c1af068aa1e124b9
Corey Walsh <matthias61984@hotmail.com>
2018-11-07 18:04:41 -0800

    second push

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,10 +12,11 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
-    afterConnection();
+    listProducts();
+    shop();
 });
 
-function afterConnection() {
+function listProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         for (i=0; i < res.length; i++) {
@@ -22,4 +24,46 @@ function afterConnection() {
         }
         connection.end();
     });
+}
+
+function shop() {
+    inquirer.prompt([
+    {
+      type: "input",
+      message: "Input the ID of the item you would like to purchase.",
+      name: "itemId"
+    },
+    {
+      type: "input",
+      message: "How many would you like to purchase?",
+      name: "number"
+    },
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    },
+  ])
+  .then(function(inquirerResponse) {
+    if (inquirerResponse.confirm) {
+      console.log("You have purchased " + inquirerResponse.number + " of item number " + inquirerResponse.itemId + ".");
+      console.log(inquirerResponse);
+      var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: stock_quantity - inquirerResponse.number
+          }
+        ],
+        function(err, res) {
+          console.log(res.affectedRows + " products updated!\n");
+        }
+      );
+    }
+    else {
+      console.log("Please enter the item number and then the amount of that item you would like to purchase.");
+      shop();
+    }
+  });
 }
